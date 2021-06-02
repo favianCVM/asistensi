0ca4789c84237906a47e99a8443359d075f252af
fabian <koe7the@gmail.com>
2021-06-02 00:39:05 -0400

    Implement redux provider and install axios package
    Also add a new color var

--- src/containers/UsersContainer.jsx
+++ src/containers/UsersContainer.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import UsersSection from "../components/UsersSection/UsersSection";
+import { useSelector, useDispatch } from "react-redux";
+import { saveUsers } from "../actions/usersActions";
+import axios from "axios";
 
 const UsersContainer = () => {
+  const usersStore = useSelector((state) => state);
+  const dispath = useDispatch();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then(({ data }) => {
+        setUsers(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="container">
       <Navbar />
-      <UsersSection />
+      <UsersSection users={users} setUsers={setUsers} />
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import users from "./reducers/users";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  users,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
